d3795220d6e953b23a7c3f56dfc879a42fec1d30
Stuart Mair <2353404m@student.gla.ac.uk>
2020-01-24 23:28:17 +0000

    Chpt4 Done -- Test PASS

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page." + '<a href="/rango/">Index</a>')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
